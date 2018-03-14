7ab26170f3712ede6f499fbaf236685a05cdf356
Maverick <embrymaverick@gmail.com>
2018-03-14 12:47:24 -0700

    styling with css

--- assets/javascript/trains.js
+++ assets/javascript/trains.js
@@ -3,52 +3,54 @@ $( document ).ready(function() {
     console.log( "ready!" );
 
   // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCUXC6y9bVNi77gWmRyYVCdjbHgGJdko6Y",
-    authDomain: "mavs-firebase.firebaseapp.com",
-    databaseURL: "https://mavs-firebase.firebaseio.com",
-    projectId: "mavs-firebase",
-    storageBucket: "mavs-firebase.appspot.com",
-    messagingSenderId: "114496442870"
-  };
-  firebase.initializeApp(config);
+//   var config = {
+//     apiKey: "AIzaSyCUXC6y9bVNi77gWmRyYVCdjbHgGJdko6Y",
+//     authDomain: "mavs-firebase.firebaseapp.com",
+//     databaseURL: "https://mavs-firebase.firebaseio.com",
+//     projectId: "mavs-firebase",
+//     storageBucket: "mavs-firebase.appspot.com",
+//     messagingSenderId: "114496442870"
+//   };
+//   firebase.initializeApp(config);
 
- var database = firebase.database();
+//  var database = firebase.database();
  
 
   $(".submit").on("click", function (){
     var name = $(".name").val().trim();
-    var role = $(".role").val().trim();
-    var start = $(".start").val().trim();
-    var rate = $(".rate").val().trim();
+    var dest = $(".dest").val().trim();
+    var time = $(".time").val().trim();
+    var freq = $(".freq").val().trim();
     console.log(name);
-    console.log(role);
-    console.log(start);
-    console.log(rate);
+    console.log(dest);
+    console.log(time);
+    console.log(freq);
 
-    database.ref().push({
-        name: name,
-        role: role,
-        start: start,
-        rate: rate,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    // database.ref().push({
+    //     name: name,
+    //     dest: dest,
+    //     time: time,
+    //     freq: freq,
+    //     dateAdded: firebase.database.ServerValue.TIMESTAMP
 
-    })
+    // })
 
-    $("<tr><td></td></tr>").text('hey');
+    $('.table tr:last').after('<tr><td>'+ name + '</td>'+'<td>'+ dest + '</td>'+ '<td>'+ time + '</td>'+'<td>'+ freq + '</td>'+'</tr>');
+
+  
    
   
      
 
-    database.ref().on("child_added", function(snapshot){
+    // database.ref().on("child_added", function(snapshot){
 
-        console.log("snapshot: ",  snapshot);
-        console.log(snapshot.val());
-        console.log(snapshot.val().name);
-        console.log(snapshot.val().role);
-        console.log(snapshot.val().start);
-        console.log(snapshot.val().rate);
-    })
+    //     console.log("snapshot: ",  snapshot);
+    //     console.log(snapshot.val());
+    //     console.log(snapshot.val().name);
+    //     console.log(snapshot.val().dest);
+    //     console.log(snapshot.val().time);
+    //     console.log(snapshot.val().freq);
+    // })
 
   });
 
@@ -58,11 +60,7 @@ $( document ).ready(function() {
  
   
 
-  $("#click-button").on("click", function() {
-    clickCounter++;
-    database.ref().set({
-      clickCount: clickCounter
-    });
-  });
+  
+ 
 
 });

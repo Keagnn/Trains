b861a4e9baa352ba6a232bc42b38c43200aa5f7c
Maverick <embrymaverick@gmail.com>
2018-03-15 20:42:00 -0700

    minor changes

--- assets/javascript/trains.js
+++ assets/javascript/trains.js
@@ -17,13 +17,14 @@ $(document).ready(function () {
 
   var timeArr = [];
 
-  
 
-  
+
 
 
 
   $(".submit").on("click", function () {
+    
+
     var name = $(".name").val().trim();
     var dest = $(".dest").val().trim();
     var time = $(".time").val().trim();
@@ -32,7 +33,7 @@ $(document).ready(function () {
     console.log(dest);
     console.log(time);
     console.log(freq);
-    
+
 
 
     // database.ref().push({
@@ -53,16 +54,16 @@ $(document).ready(function () {
     var minutes = moment.duration(currentTime).subtract(time);
     var msAway = (minutes * -1);
     var mins = moment.duration(msAway).asMinutes();
-    
+
 
 
 
     timeArr.push(mins);
-  
+
     var min = Math.min.apply(Math, timeArr);
     console.log(min);
     $('.float').html(" <marquee>The next train will arrive in " + min + " Minutes! </marquee>");
-    
+
 
 
 
@@ -93,8 +94,30 @@ $(document).ready(function () {
 
   });
 
+  
 
 
+  // function buttonCheck(){
+  //   if ($(".name").val() === "" || ($(".dest").val()=== "" || ($(".time").val()==="" || ($(".freq").val()==="")))){
+  //     console.log("You didn't fill every field!");
+  //   }
+
+  // };
+
+  $('.submit').attr('disabled',true);
+
+  $('#input').keyup(function(){
+      if($(this).val().length !=0){
+          $('.submit').prop('disabled', false);
+      }
+      else
+      {
+          $('.submit').prop('disabled', true);        
+      }
+     
+  })
+
+ 
 
 
 
